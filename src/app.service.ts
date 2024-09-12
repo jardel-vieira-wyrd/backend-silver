30f97f00f8ed1d7d102bc8e35660b8c76204b094
jardel-vieira-wyrd <jardel.vieira@wyrd.com.br>
2024-09-12 20:55:43 -0300

    Set up API

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getHealthStatus(): Promise<string> {
+    try {
+      await this.prisma.$queryRaw`SELECT 1`;
+      return 'OK';
+    } catch (error) {
+      return 'ERROR';
+    }
   }
 }
