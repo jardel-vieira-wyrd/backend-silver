d0c7a1643f277b5707ad78c30e45049526b226dd
jardel-vieira-wyrd <jardel.vieira@wyrd.com.br>
2024-09-13 23:20:10 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
+  SwaggerModule.setup('api', app, document as any);
+
   await app.listen(3000);
 }
 bootstrap();
