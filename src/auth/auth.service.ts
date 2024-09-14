366a6817f4eb10fa7650b2fd4284b6c51055035b
jardel-vieira-wyrd <jardel.vieira@wyrd.com.br>
2024-09-14 15:42:59 -0300

    fix based on frontend needs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,7 +22,11 @@ export class AuthService {
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token: this.jwtService.sign(payload),
     };
   }
 
@@ -33,6 +37,12 @@ export class AuthService {
       password: hashedPassword,
     });
     const { password, ...result } = user;
-    return result;
+    return {
+      user: {
+        name: result.name,
+        email: result.email,
+      },
+      token: this.jwtService.sign({ email: result.email, sub: result.id }),
+    };
   }
 }
